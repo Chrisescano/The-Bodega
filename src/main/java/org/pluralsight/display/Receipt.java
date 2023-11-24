62babbda51cfb9d8b65dae74d15877aceec6e35c
Christian Escano <escanochristian21@gmail.com>
2023-11-24 17:05:54 -0500

    Added some receipt functionality like timestamp Added storeInformation.csv to put into the receipt

--- src/main/java/org/pluralsight/display/Receipt.java
+++ src/main/java/org/pluralsight/display/Receipt.java
@@ -1,5 +1,26 @@
 package org.pluralsight.display;
 
-public class Receipt {
+import org.pluralsight.io.FileManager;
 
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Receipt {
+    private final String timeStamp;
+    private FileManager storeFileManager;
+    private FileManager orderFileManager;
+
+    public Receipt() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        timeStamp = LocalDateTime.now().format(dateTimeFormatter);
+
+        storeFileManager = new FileManager("");
+        orderFileManager = new FileManager("Toppings/");
+    }
+
+    /*-----Getter-----*/
+    public String getTimeStamp() {
+        return timeStamp;
+    }
 }
