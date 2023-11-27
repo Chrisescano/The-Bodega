858679784e34bfa048d00b3909cf847bf2a4d596
Christian Escano <escanochristian21@gmail.com>
2023-11-27 15:14:14 -0500

    Added Item class as an abstract super class Added RegularItem as a subclass of Item Added PremiumItem as a subclass of Item Added ItemManager to represent items as arraylist

---
+++ src/main/java/org/pluralsight/menuitems/RegularItem.java
@@ -0,0 +1,25 @@
+package org.pluralsight.menuitems;
+
+import org.pluralsight.shop.Size;
+
+public class RegularItem extends Item{
+    public RegularItem(String itemInformation) {
+        String[] itemParts = itemInformation.split("\\|");
+        itemName = itemParts[0];
+
+        itemPrices.put(Size.SMALL, Double.valueOf(itemParts[1]));
+        itemPrices.put(Size.MEDIUM, Double.valueOf(itemParts[2]));
+        itemPrices.put(Size.LARGE, Double.valueOf(itemParts[3]));
+    }
+
+    /*-----Getters-----*/
+    @Override
+    public String getItemName() {
+        return itemName;
+    }
+
+    @Override
+    public double getPrice(Size breadSize) {
+        return itemPrices.get(breadSize);
+    }
+}
