55160e0146c7af21963c0d9b57e515128ab7e5b4
karyRmartinez <karymartinez@pursuit.org>
2023-11-28 13:32:31 -0500

    changes to drink class

--- src/main/java/org/pluralsight/shop/Drink.java
+++ src/main/java/org/pluralsight/shop/Drink.java
@@ -2,10 +2,6 @@ package org.pluralsight.shop;
 
 
 
-import org.pluralsight.io.ScannerIO;
-
-
-
 import org.pluralsight.menuitems.Item;
 import org.pluralsight.menuitems.ItemManager;
 import java.util.ArrayList;
@@ -19,15 +15,14 @@ public class Drink {
 
     private ItemManager itemManager;
     private Item selectedDrink;
-
-//    private Size size;
-//    private String flavor;
     public Drink() {
-        itemManager = new ItemManager("drinks.csv", false);
+        this.itemManager = new ItemManager("drinks.csv", false);
+        //ItemManager = new ItemManager("drinks.csv", false);
     }
 
+
+
     public void selectedDrink() {
-        System.out.println("Select a drink:");
 
         ArrayList<Item> drinkOptions = itemManager.getItemList();
 
@@ -35,18 +30,8 @@ public class Drink {
             System.out.println((i + 1) + ". " + drinkOptions.get(i).getItemName());
         }
 
-        int option = ScannerIO.getIntInput("Enter the drink option: ");
-
-        if (option >= 1 && option <= drinkOptions.size()) {
-            selectedDrink = drinkOptions.get(option - 1);
-            System.out.println("Drink Selected: " + selectedDrink.getItemName());
-        } else {
-            System.out.println("Error! Tru again.");
-        }
-
     }
-
-
+    //Adding addToOrder method
 }
 
 
