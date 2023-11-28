bd38733a662a076314159432d9f2cd418a53d5f6
Christian Escano <escanochristian21@gmail.com>
2023-11-28 13:42:32 -0500

    saving work

--- src/main/java/org/pluralsight/shop/Chips.java
+++ src/main/java/org/pluralsight/shop/Chips.java
@@ -1,4 +1,21 @@
 package org.pluralsight.shop;
 
+import org.pluralsight.menuitems.Item;
+import org.pluralsight.menuitems.ItemManager;
+
 public class Chips {
+    ItemManager chipItems;
+
+    public Chips() {
+        chipItems = new ItemManager("chips.csv", false);
+    }
+
+    public void order(String itemName) {
+        for (Item chip : chipItems.getItemList()) {
+            if (chip.getItemName().equals(itemName)) chip.orderItem();
+        }
+    }
+
+    /*-----Getters-----*/
+
 }
