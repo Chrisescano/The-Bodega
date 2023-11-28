125fcc9bf2c85a5d21251e8eab0e72d76312afd1
Christian Escano <escanochristian21@gmail.com>
2023-11-28 11:40:43 -0500

    Tested Receipt class methods NOTE: testing for receipt time in seconds will kinda fail, but method works as intended'

--- src/test/java/org/pluralsight/menuitems/RegularItemTest.java
+++ src/test/java/org/pluralsight/menuitems/RegularItemTest.java
@@ -10,14 +10,14 @@ class RegularItemTest {
     @Test
     void getItemNameTest() {
         RegularItem regularItem = new RegularItem("lettuce|0.00|0.00|0.00");
-        assertEquals(regularItem.getItemName(), "lettuce");
+        assertEquals("lettuce", regularItem.getItemName());
     }
 
     @Test
     void getPriceTest() {
         RegularItem regularItem = new RegularItem("lettuce|0.00|0.00|0.00");
-        assertEquals(regularItem.getPrice(Size.SMALL), 0.00);
-        assertEquals(regularItem.getPrice(Size.MEDIUM), 0.00);
-        assertEquals(regularItem.getPrice(Size.LARGE), 0.00);
+        assertEquals(0.00, regularItem.getPrice(Size.SMALL));
+        assertEquals(0.00, regularItem.getPrice(Size.MEDIUM));
+        assertEquals(0.00, regularItem.getPrice(Size.LARGE));
     }
 }
